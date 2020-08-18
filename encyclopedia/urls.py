3054468077ca827a19386bebc13fd26f7cd3f292
ocasergi@gmail.com <ocasergi@gmail.com>
2020-08-18 22:08:48 +0200

    Results attempt

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("encyclopedia/<str:entry_name>/", views.entry, name="entry")
+    path("encyclopedia/<str:entry_name>/", views.entry, name="entry"),
+    path("encyclopedia/search/", views.search, name="search"),
+    path("encyclopedia/results/", views.results, name="results")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
 
 from . import util
+
+class NewTaskForm(forms.Form):
+    search = forms.CharField()
 
 
 def index(request):
@@ -8,6 +13,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def results(request):
+    return render(request, "encyclopedia/results.html", {
+        "entries": util.list_entries()
+    })
+
+    
 def entry(request, entry_name):
     entry = util.get_entry( entry_name )
     if entry == None :
@@ -17,6 +28,31 @@ def entry(request, entry_name):
             "entry": util.get_entry( entry_name ),
             "title": entry_name
         })
+
+def search(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            searchQuery = util.get_entry( form.cleaned_data["search"] )
+            if searchQuery == None :
+                return render(request, "encyclopedia/results.html", {
+                    "query": util.list_entries()
+                })
+            else :
+                return render(request, "encyclopedia/results.html", {
+                    searchQuery
+                })
+        else:
+            return render(request, "/", {
+                "form": form
+            })
+
+    else:
+        return render(request, "/", {
+            "form": NewTaskForm()
+        })    
+
+
 
 # def entry(request, entry_name):
 #     return render(request, "encyclopedia/entry.html", {
